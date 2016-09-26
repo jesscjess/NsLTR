a55df055d616e456148867b4ec64bbcbb16665d5
Jessy Jess Flores <mrsflores@Gabriels-MacBook-Pro.local>
2016-09-26 00:08:45 -0700

    Playing around with ng-sortable

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,28 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function ($scope) {
+	
+	$scope.itemsList = {
+        items1: [],
+        items2: []
+    };
+	
+	for (var i = 0; i <= 5; i += 1) {
+        $scope.itemsList.items1.push({'Id': i, 'Label': 'Item A_' + i});
+    }
+//
+//    for (i = 0; i <= 5; i += 1) {
+//        $scope.itemsList.items2.push({'Id': i, 'Label': 'Item B_' + i});
+//    }
 
+	$scope.sortableOptions = {
+        containment: '#sortable-container',
+        allowDuplicates: true
+    };
+    $scope.sortableCloneOptions = {
+        containment: '#sortable-container',
+        clone: true
+    };
+	
 }]);
