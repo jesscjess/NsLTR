ffc779a94ee08cebe5f75deaa8d710025d56faac
Jessica Cornejo Flores <JessyJess@Jessicas-MacBook-Air.local>
2016-11-01 00:04:13 -0700

    Continued to work on the styling of the page

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,42 +12,33 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope', function ($scope) {
 	
 	$scope.itemsList = {
-        items1: ['components/nsltrTemplate/templates/1-header.html',
-                 'components/nsltrTemplate/templates/2-confirmation.html',
-                 'components/nsltrTemplate/templates/3-message-title.html',
-                 'components/nsltrTemplate/templates/divider.html',
-                 'components/nsltrTemplate/templates/4-left-right.html',
-                 'components/nsltrTemplate/templates/5-message-title_w_img.html',
-                 'components/nsltrTemplate/templates/6-left-right_w_top-title.html',
-                 'components/nsltrTemplate/templates/7-left-right_w_bottom-title.html',
-                 'components/nsltrTemplate/templates/8-right_w_img.html',
-                 'components/nsltrTemplate/templates/9-left_w_img.html',
-                 'components/nsltrTemplate/templates/10-right_w_small-img.html',
-                 'components/nsltrTemplate/templates/11-right_w_small-img-wrap.html',
-                 'components/nsltrTemplate/templates/12-left-right-block-colour.html',
-                 'components/nsltrTemplate/templates/13-empty.html'],
-        items2: ['components/nsltrTemplate/templates/1-header.html']
+        items1: [{url:'components/nsltrTemplate/templates/1-header.html', list:1},
+                 {url:'components/nsltrTemplate/templates/2-confirmation.html', list:1},
+                 {url:'components/nsltrTemplate/templates/3-message-title.html', list:1},
+                 {url:'components/nsltrTemplate/templates/divider.html', list:1},
+                 {url:'components/nsltrTemplate/templates/4-left-right.html', list:1},
+                 {url:'components/nsltrTemplate/templates/5-message-title_w_img.html', list:1},
+                 {url:'components/nsltrTemplate/templates/6-left-right_w_top-title.html', list:1},
+                 {url:'components/nsltrTemplate/templates/7-left-right_w_bottom-title.html', list:1},
+                 {url:'components/nsltrTemplate/templates/8-right_w_img.html', list:1},
+                 {url:'components/nsltrTemplate/templates/9-left_w_img.html',list:1},
+                 {url:'components/nsltrTemplate/templates/10-right_w_small-img.html', list:1},
+                 {url:'components/nsltrTemplate/templates/11-right_w_small-img-wrap.html', list:1},
+                 {url:'components/nsltrTemplate/templates/12-left-right-block-colour.html', list:1},
+                 {url:'components/nsltrTemplate/templates/13-empty.html', list:1}], 
+        items2: [{url:'components/nsltrTemplate/templates/1-header.html', list:2}]
     };
 
-    console.log($scope.itemsList.items2);
-	
-	// for (var i = 0; i <= 5; i += 1) {
-    //     $scope.itemsList.items1.push({'Id': i, 'Label': 'Item A_' + i});
-    // }
 
-//    for (var i = 0; i <= 5; i += 1) {
-//        $scope.itemsList.items2.push({'Id': i, 'Label': 'Item B_' + i});
-//    }
-
-   $scope.url = 'components/nsltrTemplate/templates/header.html';
-
-	$scope.sortableOptions = {
+	$scope.sortableComponentOptions = {
         containment: '#sortable-container',
-        allowDuplicates: true
+        clone: true,
+        // ctrlClone: true
     };
-    $scope.sortableCloneOptions = {
+    $scope.myNsLTROptions = {
         containment: '#sortable-container',
-        clone: true
+        allowDuplicates: true,
+        ctrlClone: true
     };
 	
 }]);
